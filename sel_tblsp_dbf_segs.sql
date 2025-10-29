88206815db3ac4e5be4e5eaa7bb776ead8994b18
hfungwi <Harrisfungwi14@gmail.com>
2025-10-29 09:13:38 -0500

    Create  sel_tblsp_dbf_segs.sql
    Displays the current size of a tablespace (sum of data files current size) and the total amount of used space (size of the segments). 
    NB: max size could be greater than current size

---
+++ sel_tblsp_dbf_segs.sql
@@ -0,0 +1,12 @@
+ WITH total_space as ( SELECT tablespace_name, round(sum(bytes/1024/1024)) total_space_mb
+                        FROM   dba_data_files
+                        GROUP BY tablespace_name
+                      ),
+  used_space as ( SELECT tablespace_name, round(sum(bytes/1024/1024)) used_space_mb
+                  FROM   dba_segments
+                  GROUP BY tablespace_name
+                )
+  SELECT t.tablespace_name, t.total_space_mb, u.used_space_mb
+  FROM   total_space t INNER JOIN used_space u
+          ON t.tablespace_name = u.tablespace_name
+/
