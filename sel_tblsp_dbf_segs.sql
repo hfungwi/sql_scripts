67c63b9db058eb878eea136e6d211e594922cfb0
hfungwi <Harrisfungwi14@gmail.com>
2026-01-02 10:16:55 -0600

    Update sel_tblsp_dbf_segs.sql
    takes into consideration the maxbytes prior to computing the total space available in the tablespace

--- sel_tblsp_dbf_segs.sql
+++ sel_tblsp_dbf_segs.sql
@@ -1,12 +1,18 @@
- WITH total_space as ( SELECT tablespace_name, round(sum(bytes/1024/1024)) total_space_mb
+WITH
+ total_space as ( SELECT
+                              tablespace_name,
+                              round(sum(greatest(bytes, decode(maxbytes, 0, bytes, maxbytes))/1024/1024)) total_space_mb
                         FROM   dba_data_files
                         GROUP BY tablespace_name
                       ),
-  used_space as ( SELECT tablespace_name, round(sum(bytes/1024/1024)) used_space_mb
+  used_space as ( SELECT
+                         tablespace_name,
+                         round(sum(bytes/1024/1024)) used_space_mb
                   FROM   dba_segments
                   GROUP BY tablespace_name
                 )
   SELECT t.tablespace_name, t.total_space_mb, u.used_space_mb
-  FROM   total_space t INNER JOIN used_space u
-          ON t.tablespace_name = u.tablespace_name
+  FROM          total_space t
+     INNER JOIN used_space u  ON t.tablespace_name = u.tablespace_name
+  ORDER BY t.tablespace_name
 /
